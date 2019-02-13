172be8dccfe6fbe3ada55a3b8504a27378f84de1
han.congcong <han.congcong@immomo.com>
2019-02-13 19:19:56 +0800

    fileSystemFactory modify

--- src/main/java/com/hc/utils/hbase/Main.java
+++ src/main/java/com/hc/utils/hbase/Main.java
@@ -6,23 +6,24 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
 
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Configuration conf = HBaseConfiguration.create();
         conf.set(HConstants.ZOOKEEPER_QUORUM,"127.0.0.1");
 
         Connection connection = ConnectionFactory.createConnection(conf);
 
         HTable hTable = (HTable) connection.getTable(TableName.valueOf("TestTable"));
-        Scan scan = new Scan();
-        ResultScanner scanner = hTable.getScanner(scan);
-        for(Result result : scanner){
-            System.out.println(Bytes.toString(result.getRow()));
+        for(int i = 0;i < 100; i++){
+            Scan scan = new Scan();
+            scan.setAsyncPrefetch(true);
+            ResultScanner scanner = hTable.getScanner(scan);
+            scanner.next();
         }
+        Thread.sleep(1000000);
     }
 }
--- src/main/java/com/hc/utils/hdfs/FileSystemFactory.java
+++ src/main/java/com/hc/utils/hdfs/FileSystemFactory.java
@@ -7,10 +7,14 @@ import org.apache.hadoop.fs.FileSystem;
 import java.io.IOException;
 
 public class FileSystemFactory {
+    private static final String FS_DEFAULT_FS = "fs.defaultFS";
+
     public static FileSystem getFileSystem(String url) throws IOException {
+
         Configuration configuration = new Configuration();
-        configuration.set("fs.defaultFS", "hdfs://localhost:8020");
+        configuration.set(FS_DEFAULT_FS, url);
         FileSystem fs = FileSystem.get(configuration);
         return fs;
+
     }
 }
