1d7bed024679ed3a698b8e1f54499787efb5b89c
han.congcong <han.congcong@immomo.com>
2019-09-24 22:42:13 +0800

    add traces

---
+++ src/main/java/com/hc/utils/Traces.java
@@ -0,0 +1,50 @@
+package com.hc.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+/**
+ * Traces
+ *
+ * @author han.congcong
+ * @date 2019/9/24
+ */
+
+public class Traces {
+
+    private static ThreadLocal<Boolean> TURN = ThreadLocal.withInitial(() -> Boolean.FALSE);
+    private static ThreadLocal<Map<String,Object>> TRACE = ThreadLocal.withInitial(HashMap::new);
+
+    public static void turnOn(){
+        TURN.set(Boolean.TRUE);
+    }
+
+    public static void turnOff(){
+        TURN.set(Boolean.FALSE);
+    }
+
+    public static void add(String key, Object value, Function function){
+        if(TURN.get()) {
+            TRACE.get().put(key, function.apply(value));
+        }
+    }
+
+    public static void add(String key,Object value){
+        if(TURN.get()) {
+            TRACE.get().put(key, value);
+        }
+    }
+
+    public static Map<String,Object> get(){
+        return TRACE.get();
+    }
+
+    public static Object get(String key){
+        return TRACE.get().get(key);
+    }
+
+    public static void remove(){
+        TRACE.remove();
+    }
+}
---
+++ src/test/java/Simple.java
@@ -0,0 +1,14 @@
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Simple
+ *
+ * @author han.congcong
+ * @date 2019/8/27
+ */
+
+public class Simple {
+    public static void main(String[] args){
+        System.out.println(TimeUnit.HOURS.toDays(1));
+    }
+}
